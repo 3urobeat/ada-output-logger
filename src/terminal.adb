2ccbe1b94c30fc994bbac94cc9e391c2dcf338a6
3urobeat <35304405+3urobeat@users.noreply.github.com>
2024-07-26 21:03:58 +0200

    feat: Add function to get Linux terminal width

---
+++ src/terminal.adb
@@ -0,0 +1,33 @@
+-- File: terminal.adb
+-- Project: ada-output-logger
+-- Created Date: 2024-07-26 20:44:52
+-- Author: 3urobeat
+--
+-- Last Modified: 2024-07-26 21:02:44
+-- Modified By: 3urobeat
+--
+-- Copyright (c) 2024 3urobeat <https://github.com/3urobeat>
+--
+-- This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+-- This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+-- You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+package body Terminal is
+
+   -- Gets the width (columns) of the current terminal (supports only Linux atm)
+   function Get_Terminal_Width return Positive is
+      Window_Size : Terminal_Dimensions_Type;
+      Std_Input_FD : constant C.int := 0;  -- File Descriptor for stdin
+   begin
+
+      -- Attempt to fill Window_Size with data from Ioctl. Throw error if Ioctl did not succeed.
+      if Ioctl(Std_Input_FD, TIOCGWINSZ, Window_Size'Address) /= 0 then
+         raise Program_Error;
+      end if;
+
+      return Positive(Window_Size.Cols);
+
+   end Get_Terminal_Width;
+
+end Terminal;
---
+++ src/terminal.ads
@@ -0,0 +1,50 @@
+-- File: terminal.ads
+-- Project: ada-output-logger
+-- Created Date: 2024-07-26 20:44:52
+-- Author: 3urobeat
+--
+-- Last Modified: 2024-07-26 21:02:44
+-- Modified By: 3urobeat
+--
+-- Copyright (c) 2024 3urobeat <https://github.com/3urobeat>
+--
+-- This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+-- This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+-- You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+with Interfaces.C;
+with System;
+
+
+package Terminal is
+
+   -- Gets the width (columns) of the current terminal (supports only Linux atm)
+   -- @return Returns the amount of columns of the current terminal
+   function Get_Terminal_Width return Positive;
+
+private
+
+   -- TODO: Can this be shortened?
+   -- TODO: Add windows support and surround it with pre-processor macros
+
+   -- Get datatypes from C to pass to Ioctl
+   package C renames Interfaces.C;
+   use type C.int;
+   use type C.short;
+
+   -- Stores data returned by Ioctl
+   type Terminal_Dimensions_Type is record
+      Rows : C.short;
+      Cols : C.short;
+      Xpixel : C.short;
+      Ypixel : C.short;
+   end record;
+   pragma Convention (C, Terminal_Dimensions_Type);
+
+   TIOCGWINSZ : constant C.int := 16#5413#;
+
+   function Ioctl (Fd : C.int; Request : C.int; Arg : System.Address) return C.int;
+   pragma Import (C, Ioctl, "ioctl");
+
+end Terminal;
